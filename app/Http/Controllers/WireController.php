2751b09f4c47b0d5a8de973b3c1ed6cbdc2fcb25
elisvasconcelos <elis.v.lopes@gmail.com>
2020-07-30 21:13:55 -0300

    02
    add autenticação

--- app/Http/Controllers/WireController.php
+++ app/Http/Controllers/WireController.php
@@ -17,27 +17,26 @@ class WireController extends Controller
 
     //realizar busca na API
     public function search()
-    {
-    	/*$auth = Http::get('https://accounts.spotify.com/authorize', [
-	        'client_id' => 'a5b4340160134aefa13b31e340975eb0',
-	        'response_type' => 'token',
-	        'redirect_uri' => 'http://127.0.0.1:8000/search?',
-	        'scope' => 'user-read-private%20user-read-email',
-	        'state' => '34fFs29kd09',
-	        'show_dialog' => 'false',
-	    ]);*/
+    {       
+        $auth = Http::withHeaders([
+          'Authorization' => 'Basic YTViNDM0MDE2MDEzNGFlZmExM2IzMWUzNDA5NzVlYjA6ZTk3NmUyNGY2NTcxNDQ0NTliZmNkMjlmOTlmNjY3OTM='
+        ])->asForm()->post('https://accounts.spotify.com/api/token', [
+            'grant_type' => 'client_credentials',
+        ]);
+        $obj = $auth->json();
+        $token = $obj['access_token'];
+        
+        $busca = Request::get('busca');
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer '.$token,
+        ])->get('https://api.spotify.com/v1/search?', [
+            'q' => $busca,
+            'type' => 'track',
+        ]);
 
-      	$busca = Request::get('busca');
-      	$response = Http::withHeaders([
-        	'Authorization' => 'Bearer BQCrC3W8EWWUtvhP5Dn71Glt1pXWKlEoucHqSLuXwml3l1umNpOkvCPLjc6glJclWtMQ2W1gL12UvQesdyREGCQHppuiHXyrCGBp374t4SqdlVDuvm1pV7DH0_kQwWmxp_6p13X-2IjrNLnyusN9zHTPKEVbIsjSviWVCIuvLchQPGFtvxwUkbNoIt2_J0lb7VcrDi92qWnKLtDRgMi0uSCQ3I0eMRTAjwNm5ekn7hSvSC0JHGN41S2xBvwZTqJ4NUND2_89msj4'
-    	])->get('https://api.spotify.com/v1/search?', [
-        	'q' => $busca,
-        	'type' => 'track',
-    	]);
+        $resp = $response->json();
 
-    	$resp = $response->json();
-
-    	return $resp;
+        return $resp;
     }
 }
 
